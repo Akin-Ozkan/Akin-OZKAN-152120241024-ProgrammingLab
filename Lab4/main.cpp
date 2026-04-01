a1c5dd298df3dc1ba509ba0b44b2e05581adae63
Akin-Ozkan <akinozkann@proton.me>
2026-04-01 11:29:21 +0300

    Proje fonksiyonları oluşturuldu. Henüz döndürecekleri değer yazılmadı.

---
+++ Lab4/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+struct Package(){
+    string packageID;
+    string destinationCity;
+    int dimensions[3];
+    Package* next;
+};
+
+struct Courier{
+    string courierName;
+    string vehicleType;
+    int front;
+    int rear;
+
+};
+
+
+push()
+{
+
+}
+
+enqueue()
+{
+
+}
+
+pop()
+{
+
+}
+
+display()
+{
+
+}
+
+
+int main()
+{
+
+    return 0;
+}
